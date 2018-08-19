749ec05c341a46b915b2b0287c7616d42e0f3e03
Aneesh Abraham <AABRAHAM@amfam.com>
2018-08-18 21:55:16 -0500

    SessionURLTask with JSON Serialization is implemented

--- Refresher-SWIFT/Controller/Concepts/Networking/SessionDataTaskViewController.swift
+++ Refresher-SWIFT/Controller/Concepts/Networking/SessionDataTaskViewController.swift
@@ -9,27 +9,40 @@
 import UIKit
 
 class SessionDataTaskViewController: UIViewController {
-
+    
+    @IBOutlet weak var resultTextView: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        resultTextView.text = "Loading"
         // Do any additional setup after loading the view.
+        getData()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func getData() {
+        let session = URLSession.shared
+        let url = URL(string: "https://itunes.apple.com/search?media=music&entity=song&term=crowded+house")
+        let dataTask = session.dataTask(with: url!) { (data, response, error) in
+            var responseString = ""
+            guard let data = data, error == nil else { return }
+            do {
+                let json = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as! [String:Any]
+                let posts = json["results"] as? [[String: Any]] ?? []
+                dump(posts)
+                for (key,value) in json {
+                    responseString = responseString + ("\(key) = \(value)")
+                }
+                DispatchQueue.main.async {
+                    self.resultTextView.text = responseString
+                }
+            } catch let error as NSError {
+                print(error)
+            }
+        }
+        dataTask.resume()
     }
-    */
-
 }
