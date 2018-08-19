391149bdaf04af486aa65b559a4f50d9e516bd42
Aneesh Abraham <AABRAHAM@amfam.com>
2018-08-19 01:03:48 -0500

    Implemented sample for POST by JSON codable.

--- Refresher-SWIFT/Controller/Concepts/Networking/SessionDataTaskCodablePostViewController.swift
+++ Refresher-SWIFT/Controller/Concepts/Networking/SessionDataTaskCodablePostViewController.swift
@@ -9,27 +9,53 @@
 import UIKit
 
 class SessionDataTaskCodablePostViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        getData()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func getData() {
+        let config = URLSessionConfiguration.default
+        config.waitsForConnectivity = true
+        let defaultSession = URLSession(configuration: config)
+        // Set up a local http server using https://github.com/typicode/json-server and allow http arbitrary loads in info.plist
+        let url = URL(string: "http://localhost:3000/posts")
+        var request = URLRequest(url: url!)
+        request.cachePolicy = .reloadIgnoringLocalAndRemoteCacheData
+        request.networkServiceType = .background
+        request.allowsCellularAccess = true
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "content-type")
+        
+        let encoder = JSONEncoder()
+        let post = Post(author: "meme", title: "tuttu")
+        do {
+            let data = try encoder.encode(post)
+            request.httpBody = data
+        } catch let encodeError as NSError {
+            print("Encode Error:\(encodeError.localizedDescription)")
+        }
+        
+        let dataTask = defaultSession.dataTask(with: request) { (data, response, error) in
+            
+            guard let data = data, let response = response as? HTTPURLResponse, response.statusCode == 201 else {
+                return
+            }
+            do {
+                let jsonDecoder = JSONDecoder()
+                let results = try jsonDecoder.decode(Post.self, from: data)
+                dump(results)
+            } catch let decodeError as NSError {
+                print(decodeError.localizedDescription)
+            }
+        }
+        dataTask.resume()
     }
-    */
+}
 
+struct Post : Codable {
+    let author : String
+    let title : String
 }
