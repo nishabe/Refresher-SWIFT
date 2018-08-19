ec64a2b09c4a0081f300cf532864ac72a8521fea
Aneesh Abraham <AABRAHAM@amfam.com>
2018-08-18 22:32:41 -0500

    Example for Decodable implemented

---
+++ Refresher-SWIFT/Controller/Concepts/Networking/CodableViewController.swift
@@ -0,0 +1,54 @@
+//
+//  CodableViewController.swift
+//  Refresher-SWIFT
+//
+//  Created by Abraham, Aneesh on 8/18/18.
+//  Copyright © 2018 Ammini Inc. All rights reserved.
+//
+
+import UIKit
+
+class CodableViewController: UIViewController {
+    
+    @IBOutlet weak var resultTextView: UITextView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        getData()
+    }
+    func getData() {
+        let config = URLSessionConfiguration.default
+        config.waitsForConnectivity = true
+        let defaultSession = URLSession(configuration: config)
+        
+        let url = URL(string: "https://itunes.apple.com/search?media=music&entity=song&term=crowded+house")
+        let dataTask = defaultSession.dataTask(with: url!) { (data, response, error) in
+            if let error = error {
+                self.resultTextView.text = "Error:\(error.localizedDescription)"
+            } else if let data = data, let response = response as? HTTPURLResponse, response.statusCode == 200 {
+                do {
+                    let jsonDecoder = JSONDecoder()
+                    let results = try jsonDecoder.decode(TrackList.self, from: data)
+                    dump(results)
+                    DispatchQueue.main.async {
+                        self.resultTextView.text = "Check Console output"
+                    }
+                } catch let decodeError as NSError {
+                    print(decodeError.localizedDescription)
+                }
+            }
+        }
+        dataTask.resume()
+    }
+}
+
+struct TrackList : Decodable {
+    let results : [Track]
+}
+
+struct Track : Decodable {
+    let trackName : String
+    let artistName : String
+    let previewUrl : String
+    
+}
